801a38a844cbcb7cc1f5d7462355972a6d245471
malek-laatiri <malek.laatiri73@gmail.com>
2021-07-04 20:20:22 +0100

    car migration

--- cars/app/car.php
+++ cars/app/car.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class car extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'brand',
+        'price',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
