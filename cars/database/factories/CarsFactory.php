7f03922cc8195068ab8e1de970ac50402bd0b8af
malek-laatiri <malek.laatiri73@gmail.com>
2021-07-06 20:30:04 +0100

    models

---
+++ cars/database/factories/CarsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\cars;
+use Faker\Generator as Faker;
+
+$factory->define(cars::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ cars/database/factories/FileFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\file;
+use Faker\Generator as Faker;
+
+$factory->define(file::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
