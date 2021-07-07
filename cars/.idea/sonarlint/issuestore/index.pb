14e146d14ee81904ba0a3d77a1093e9477dde39a
malek-laatiri <malek.laatiri73@gmail.com>
2021-07-07 23:54:26 +0100

    models

---
+++ cars/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+4
+.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+k
+;database/migrations/2021_07_06_202759_create_cars_table.php,0/a/0a49e9d9dea66c804ec906acca2cff9be447ac81
--- cars/database/migrations/2021_07_06_202759_create_cars_table.php
+++ cars/database/migrations/2021_07_06_202759_create_cars_table.php
@@ -13,7 +13,7 @@ class CreateCarsTable extends Migration
      */
     public function up()
     {
-        Schema::create('cars', function (Blueprint $table) {
+         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('brand_id')->constrained()->onDelete('cascade');
