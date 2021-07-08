82f5f74e554169b532fb16039a12aa3608424b3a
malek-laatiri <malek.laatiri73@gmail.com>
2021-07-08 20:17:17 +0100

    relations

--- cars/app/Http/Controllers/BrandController.php
+++ cars/app/Http/Controllers/BrandController.php
@@ -14,7 +14,8 @@ class BrandController extends Controller
      */
     public function index()
     {
-        //
+        $brands=Brand::get();
+        return view('brands.index',$brands);
     }
 
     /**
